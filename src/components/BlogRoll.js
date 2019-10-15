960db742a4d3a14508141ddce6aeff28e95c5b27
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2019-10-15 14:06:23 +0900

    ブログが保存されるディレクトリ構成の見直しとウェブページのタイトルと概要を変更

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,7 +8,7 @@ class BlogRoll extends React.Component {
     const { data } = this.props
     const categoryPath = this.props.state
     const { edges: posts } = data.allMarkdownRemark
-    console.log(posts[0]);
+    console.log(posts[0])
     console.log(categoryPath)
       
     return (
