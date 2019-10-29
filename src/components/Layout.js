439cd8e80378bad85659a9439d90c070aa0e1eba
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2019-10-29 14:49:56 +0900

    ナビバーの配置大幅変更

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,125 +1,55 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import Navbar from '../components/Navbar'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from "gatsby"
-import { Link, StaticQuery, graphql } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import Navbar from '../components/Navbar'
+import useSiteMetadata from './SiteMetadata'
+import './all.sass'
 
-class TemplateWrapper extends React.Component {
+export default class TemplateWrapper extends React.Component {
   render() {
-    const { data } = this.props
     const { title, description } = useSiteMetadata
-    const image = this.props.state==="index" ? data.main : data.sub
-    
-  return (
-    <div>
-      <Helmet>
-        <html lang="ja" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
+    return (
+      <div>
+        <Helmet>
+          <html lang="ja" />
+          <title>{title}</title>
+          <meta name="description" content={description} />
 
-        {//ファビコン画像をあとで設定すること
-         /*
-         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
+          {/*ファビコン画像をあとで設定すること
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}img/favicon-32x32.png`}
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}img/favicon-16x16.png`}
+            sizes="16x16"
+          />
 
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        */}
+          <link
+            rel="mask-icon"
+            href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+            color="#ff4400"
+          />
+          */}
 
-        <meta name="theme-color" content="#fff" />
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
-      </Helmet>
-      <div className="main-image">
-        <PreviewCompatibleImage  imageInfo={{image: image, alt:"kinako"}}/>
-        <Link to="/">
-          <h1 className="main-text">はむっと！</h1>
-        </Link>
-        <Link to="/about">
-          <h3 className="about-text">
-            <span role="img" aria-label="ham">🐹</span>
-            このブログについて
-          </h3>
-        </Link>
-        <Link to="/contact">
-          <h3 className="contact-text">
-            <span role="img" aria-label="ham">🐹</span>
-            お問い合わせ
-          </h3>
-        </Link>
-        <Link to="/hamz">
-          <div 
-            style={{
-              position: 'absolute',
-              top: '60%',
-              right: '10%',
-              width: '50px',
-              display: 'inline-block'
-            }}
-          >
-            <PreviewCompatibleImage  imageInfo={{image: data.hamz, alt:"kinako"}}/>
-          </div>
-          <div className="hamz-text">はむちゃんず</div>
-        </Link>
+          <meta name="theme-color" content="#fff" />
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content="/" />
+          <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
+        </Helmet>
+        <Navbar state={this.props.state}/>
+        {this.props.children}
       </div>
-      <Navbar />
-      {this.props.children}
-    </div>
-  )
+    )
   }
 }
-
-
-export default ({ children, state }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        main:file(relativePath: {eq: "kinako.jpg"}) {
-          childImageSharp{
-            fluid(maxWidth: 1000, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        sub:file(relativePath: {eq: "kinako2.jpg"}) {
-          childImageSharp{
-            fluid(maxWidth: 1000, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        hamz:file(relativePath: {eq: "kinako3.JPG"}) {
-          childImageSharp{
-            fluid(maxWidth: 100, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={(data) => <TemplateWrapper data={data} children={children} state={state} />}
-  />
-)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+import './all.sass'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -7,6 +9,8 @@ const Navbar = class extends React.Component {
     this.state = {
       active: false,
       navBarActiveClass: '',
+      drawerActiveClass: '',
+      importantTextActiveClass: 'navbar-layout',
     }
   }
 
@@ -22,50 +26,130 @@ const Navbar = class extends React.Component {
         this.state.active
           ? this.setState({
               navBarActiveClass: 'is-active',
+              drawerActiveClass: 'drawer-active',
+              importantTextActiveClass: '',
             })
           : this.setState({
               navBarActiveClass: '',
+              drawerActiveClass: '',
+              importantTextActiveClass: 'navbar-layout',
             })
       }
     )
   }
 
   render() {
+    const { data } = this.props
+    const image = this.props.state==="index" ? data.main : data.sub
+    const blogNavBar = this.props.state==='index' ? 'index-position': 'nav-position'
+
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        {/*<div className="container">*/}
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
+      <div className="main-navbar">
+        <PreviewCompatibleImage  imageInfo={{image: image, alt:"kinako"}}/>
+        <Link to="/">
+          <h1 className="main-text">はむっと！</h1>
+        </Link>
+        <div className={`burger-position ${this.state.drawerActiveClass}`}>
+          <div
+            className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+            data-target="navMenu"
+            onClick={() => this.toggleHamburger()}
+          >
+            <span/>
+            <span/>
+            <span/>
+          </div>
+           
           <div
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
-            <Link className="navbar-item" to="/blog/ham" state={{ path: "ham" }}>
-                はむ日和
+            { this.state.active &&
+              <>
+                <Link className="navbar-item" to="/blog/ham" state={{ path: "ham" }}>
+                  はむ日和
+                </Link>
+                <Link className="navbar-item" to="/blog/owner" state={{ path: "owner" }}>
+                  飼い主日和
+                </Link>
+                <Link className="navbar-item" to="/blog/story" state={{ path: "story" }}>
+                  ネタ日和
+                </Link>
+                <Link className="navbar-item" to="/hamz" state={{ path: "hamz" }}>
+                  うちのはむちゃんず
+                </Link>
+              </>
+            }
+            <Link className={`navbar-item ${this.state.importantTextActiveClass}`} to="/about">
+              <span role="img" aria-label="ham">🐹</span>
+              このブログについて
             </Link>
-            <Link className="navbar-item" to="/blog/owner" state={{ path: "owner" }}>
-                飼い主日和
-            </Link>
-            <Link className="navbar-item" to="/blog/story" state={{ path: "story" }}>
-                ネタ日和
+            <Link className={`navbar-item ${this.state.importantTextActiveClass}`} to="/contact">
+              <span role="img" aria-label="ham">🐹</span>
+              お問い合わせ
             </Link>
           </div>
-        {/*</div>*/}
-      </nav>
+        </div>
+        { this.state.active==false &&
+          <div className={blogNavBar}>
+            <div
+              id="navMenu"
+              className="navbar-menu"
+            >
+              <Link className="navbar-item navbar-layout" to="/blog/ham" state={{ path: "ham" }}>
+                はむ日和
+              </Link>
+              <Link className="navbar-item navbar-layout" to="/blog/owner" state={{ path: "owner" }}>
+                飼い主日和
+              </Link>
+              <Link className="navbar-item navbar-layout" to="/blog/story" state={{ path: "story" }}>
+                ネタ日和
+              </Link>
+              <Link to="/hamz">
+                <div 
+                  style={{
+                    width: '50px',
+                    display: 'inline-block'
+                  }}
+                >
+                  <PreviewCompatibleImage  imageInfo={{image: data.hamz, alt:"kinako"}}/>
+                </div>
+              </Link>
+            </div>
+          </div>
+        }
+      </div>
     )
   }
 }
 
-export default Navbar
+export default ({ state }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        main:file(relativePath: {eq: "kinako.jpg"}) {
+          childImageSharp{
+            fluid(maxWidth: 1000, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        sub:file(relativePath: {eq: "kinako2.jpg"}) {
+          childImageSharp{
+            fluid(maxWidth: 1000, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        hamz:file(relativePath: {eq: "kinako3.JPG"}) {
+          childImageSharp{
+            fluid(maxWidth: 100, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => <Navbar data={data} state={state} />}
+  />
+)
