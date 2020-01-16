fbb56e86df12fe6237dfaafedfe74ea4facd2ecf
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2020-01-16 09:40:04 +0900

    使用するはずの画像を間違えて削除してたので復活させたのとgraphqlを使わない形で画像を表示できるようにした

--- src/components/PathLayout.js
+++ src/components/PathLayout.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link, graphql, StaticQuery } from 'gatsby'
+import { Link } from 'gatsby'
+import homeImage from '../img/path/home.png'
 
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 const PathLayout = ({
-  homeImage,
-  path,
-  text
+  layoutInfo,
 }) => {
+  const { path, text } = layoutInfo
   return(
     <div className={path}>
       <div className="home-size">
@@ -28,23 +28,4 @@ const PathLayout = ({
   )
 }
 
-export default ({ layoutInfo }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        home:file(relativePath: {eq: "home5.png"}) {
-          childImageSharp {
-            fluid(maxWidth: 1000, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={(data) => {
-      const { path, text } = layoutInfo
-      
-      return <PathLayout homeImage={data.home} path={path} text={text} />
-    }}
-  />
-)
+export default PathLayout
