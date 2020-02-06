26e71d7434412062a5b79811160b3b5397dd507e
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2020-02-06 14:11:01 +0900

    ハンバーガーアイコンの色だけ変更

--- backup/Header.js
+++ backup/Header.js
@@ -281,6 +281,17 @@ const Header = class extends React.Component {
                 </>
               }
             </div>
+            {/*<input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" />
+              <label for="openSidebarMenu" class="sidebarIconToggle">
+                  
+            <div class="spinner diagonal part-1"></div>
+            <div class="spinner horizontal"></div>
+            <div class="spinner diagonal part-2"></div>
+            </label>
+             <div id="sidebarMenu">
+                 
+             </div>
+            */}
           </>
         )
       }}
